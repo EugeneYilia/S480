eb6561b261caecd2699bf15de1eca95680c29f48
yichenliu <583076221@qq.com>
2025-07-07 22:41:53 +0800

    reverse k linked list node

--- src/yichen/ReverseKLinkedList.kt
+++ src/yichen/ReverseKLinkedList.kt
@@ -14,19 +14,43 @@ class ReverseKLinkedList {
             return head
         }
 
-        while (true){
-            val first = head
+        var first = head
+        var batchFirst = head
+        var isFirst = true
+        var last: ListNode? = null
 
-            var last: ListNode? = first;
+        while (true){
+            var currentLast: ListNode? = batchFirst
 
             repeat(k - 1){
-                last = last?.next
-                if(last == null){
-                    return@repeat
+                currentLast = currentLast?.next
+                if(currentLast == null){
+                    return first
                 }
             }
 
+            if(isFirst){
+                isFirst = false
+                first = currentLast
+            }
 
+            if(last != null){
+                last.next = currentLast
+            }
+
+            var current = batchFirst
+            var next = current!!.next
+            // k=2  repeat 1
+            repeat(k - 1){
+                val oldNode = current
+                current = next
+                next = current?.next
+                current!!.next = oldNode
+            }
+
+            batchFirst!!.next = next
+            last = batchFirst
+            batchFirst = next
         }
     }
 }
