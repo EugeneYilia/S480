781ab47799e99ada9ffc1b405df431e249dee4c5
yichenliu <583076221@qq.com>
2025-07-13 22:50:49 +0800

    findLargetValidFeatureSet

---
+++ src/yichen/LargestValidFeatureSet.kt
@@ -0,0 +1,27 @@
+package yichen
+
+class LargestValidFeatureSet {
+
+    // 7/15
+    fun findLargetValidFeatureSet(feature1: Array<Int>, feature2: Array<Int>): Int {
+        val size = feature1.size
+
+        val trendUp = IntArray(size){1}
+        val trendDown = IntArray(size){1}
+
+        for(i in 1 until size){
+            for(j in 0 until i){
+                val f1Delta = feature1[i] - feature1[j]
+                val f2Delta = feature2[i] - feature2[j]
+
+                if(f1Delta > 0 && f2Delta > 0){
+                    trendUp[i] = maxOf(trendUp[i], trendUp[j] + 1)
+                } else if(f1Delta < 0 && f2Delta < 0){
+                    trendDown[i] = maxOf(trendDown[i], trendDown[j] + 1)
+                }
+            }
+        }
+
+        return maxOf(trendUp.max(), trendDown.max())
+    }
+}
