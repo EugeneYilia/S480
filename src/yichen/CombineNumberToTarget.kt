22a7cea1172e3e78b4d8d5f3ff22b519511a6c3d
yichenliu <583076221@qq.com>
2025-07-20 20:28:59 +0800

    combineNumberTo100

--- src/yichen/CombineNumberToTarget.kt
+++ src/yichen/CombineNumberToTarget.kt
@@ -1,7 +1,12 @@
 package yichen
 
 class CombineNumberToTarget {
-    fun combineNumberTo100(){
+    // 1 + 2
+    // 1 - 2
+    // 12
+    // 1 2 3 4 5 6 7 8 9    =>  100
+    // 1 + 2 + 34 - 5 + 67 - 8 + 9 = 100
+    fun combineNumberTo100(): List<String>{
 
     }
 }
