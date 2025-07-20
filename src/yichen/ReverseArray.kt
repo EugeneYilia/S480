b6d1577cf865026c14cbf42424582b4874a2fc27
yichenliu <583076221@qq.com>
2025-07-20 17:58:33 +0800

    rotate array

--- src/yichen/ReverseArray.kt
+++ src/yichen/ReverseArray.kt
@@ -1,7 +1,5 @@
 package yichen
 
-import kotlin.rem
-
 class ReverseArrayLastK {
     // 最快 但是费空间
     fun reverseKArray(src: IntArray, k: Int): IntArray {
@@ -12,6 +10,10 @@ class ReverseArrayLastK {
         return src.copyOfRange(src.size - reverseCount, src.size) + src.copyOfRange(0, src.size - reverseCount)
     }
 
+    fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
+
     // count记录
     // 空间复杂度O(1)  时间复杂度最差O(n)
     fun rotate(nums: IntArray, k: Int): Unit {
@@ -19,10 +21,32 @@ class ReverseArrayLastK {
             return
         }
 
+        val rotateCount = k % nums.size
+        for (index in 0 until gcd(k, nums.size)) {
+            var newIndex = index + rotateCount
+            var lastElement = nums[index]
+            while (newIndex != index) {
+                val newElement = nums[newIndex]
+                nums[newIndex] = lastElement
+                newIndex = (newIndex + rotateCount) % nums.size
+
+                lastElement = newElement
+            }
+            nums[index] = lastElement
+        }
+    }
+
+    // count记录
+    // 空间复杂度O(1)  时间复杂度最差O(n)
+    fun rotate2(nums: IntArray, k: Int): Unit {
+        if (nums.size <= 1 || k == nums.size || k <= 0) {
+            return
+        }
+
         var count = 0
         val rotateCount = k % nums.size
         for (index in 0 until rotateCount) {
-            if(count == nums.size) {
+            if (count == nums.size) {
                 return
             }
             var newIndex = index + rotateCount
@@ -44,7 +68,7 @@ class ReverseArrayLastK {
 fun main() {
 //    ReverseArrayLastK().reverseKArray(intArrayOf(1, 2, 3, 4, 5, 6, 7, 8), 3).contentToString()
 
-    val src = intArrayOf(1,2,3,4,5,6,7)
+    val src = intArrayOf(1, 2, 3, 4, 5, 6, 7)
     ReverseArrayLastK().rotate(src, 3)
     println(src.contentToString())
 }
