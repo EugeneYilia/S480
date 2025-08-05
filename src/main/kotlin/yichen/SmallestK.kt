f76c122a66e2cb7cabb0459ce7026d1de5d6cb2a
yichenliu <583076221@qq.com>
2025-08-05 10:31:57 +0800

    SmallestK and quicksort

---
+++ src/main/kotlin/yichen/SmallestK.kt
@@ -0,0 +1,41 @@
+package yichen
+
+class SmallestK {
+    fun smallestK(arr: IntArray, k: Int): IntArray {
+        quickSort(arr, 0, arr.size - 1)
+        return arr.copyOfRange(0, k)
+    }
+
+    fun quickSort(arr: IntArray, left: Int, right: Int) {
+        if (left >= right) {
+            return
+        }
+
+        val pivot = arr[left]
+
+        var l = left + 1
+        var r = right
+
+        while (true) {
+            while (r >= l && arr[r] >= pivot) {
+                r--
+            }
+            while (r >= l && arr[l] < pivot) {
+                l++
+            }
+
+            if (r >= l) {
+                val temp = arr[r]
+                arr[r] = arr[l]
+                arr[l] = temp
+            } else {
+                val temp = arr[r]
+                arr[r] = arr[left]
+                arr[left] = temp
+                break
+            }
+        }
+        quickSort(arr, left, r - 1)
+        quickSort(arr, r + 1, right)
+    }
+}
