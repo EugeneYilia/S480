28c1f791d7031dacaf6df4f2456257c2d8732c79
Yichen Liu <yichenliu@microsoft.com>
2025-08-29 13:38:34 +0800

    comment

--- src/main/kotlin/JiaoJiao/RemoveRedundant.kt
+++ src/main/kotlin/JiaoJiao/RemoveRedundant.kt
@@ -1,5 +1,37 @@
 package JiaoJiao
 
+
+
 fun removeDuplicateLetters(s: String): String {
 
+    val slowFindList = mutableListOf<Int>()
+    val quickFindList = mutableSetOf<Int>()
+    var finalString = ""
+
+    for (c in s.reversed()) {
+        val number = c - 'a' + 1
+        if (number in quickFindList) {
+            val compareList = slowFindList.toMutableList()
+            compareList.remove(number)
+            compareList.add(number)
+            val resultOldString = slowFindList.reversed().joinToString("")
+            val resultNewString = compareList.reversed().joinToString("")
+            val resultNumNew = resultNewString.toInt()
+            val resultNumOld = resultOldString.toInt()
+
+            if (resultNumOld >= resultNumNew) {
+                slowFindList.remove(number)
+                slowFindList.add(number)
+                finalString = finalString.replace(c.toString(), "")
+                finalString += c
+            }
+
+
+        } else {
+            quickFindList.add(number)
+            slowFindList.add(number)
+            finalString += c
+        }
+    }
+    return finalString.reversed()
 }
