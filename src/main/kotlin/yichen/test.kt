1cc0e0cf14c35efc2f4ff765a6e59ace86f1de5c
Yichen Liu <yichenliu@microsoft.com>
2025-09-06 11:49:28 +0800

    move window

---
+++ src/main/kotlin/yichen/test.kt
@@ -0,0 +1,32 @@
+package yichen
+
+class Request{
+    fun run(){}
+}
+
+// 限流器，每秒处理十个请求，使用基于请求的滑动窗口而不是自然时间的，比如请求开始处理的时间开始滑动
+class RateLimiter(var requestBuffer: ArrayList<Request>) {
+
+    var requestTime: ArrayList<Long> = ArrayList()
+
+    fun receiveRequest(request: Request){
+        this.requestBuffer.add(request)
+    }
+
+    fun processRequest(){
+        while (true) {
+            val currentTimeMills = System.currentTimeMillis()
+
+            if (requestTime.size == 10 && currentTimeMills - requestTime.first < 1000) {
+                continue
+            }
+
+            requestBuffer.removeFirst().run()
+
+            requestTime.add(currentTimeMills)
+            while (requestTime.size > 10) {
+                requestTime.removeFirst()
+            }
+        }
+    }
+}
