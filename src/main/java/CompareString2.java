b1b465bccabb5659fdfcf0fcb1420eae8da790b7
Yichen Liu <yichenliu@microsoft.com>
2025-09-14 01:06:39 +0800

    compareString  isIsomorphic

--- src/main/java/CompareString2.java
+++ src/main/java/CompareString2.java
@@ -4,17 +4,32 @@ public class CompareString2 {
             return false;
         }
 
-        int[] mapST = new int[26];
-        int[] mapTS = new int[26];
+        int[] mapST = new int[256];
+        int[] mapTS = new int[256];
 
         for(int i=0;i<s.length();i++){
-            int indexS = s.charAt(i) - 'a';
-            int indexT = t.charAt(i) - 'a';
-            mapST[indexT]++;
-            mapTS[indexS]++;
+            int indexS = s.charAt(i);
+            int indexT = t.charAt(i);
 
-            if(mapST[indexT] != mapTS[indexS]){
-                return false;
+            if(mapST[indexS] != 0){
+                if(mapST[indexS] != indexT + 1){
+                    return false;
+                }
+            } else {
+                // a - a
+                // 0 - 1
+                mapST[indexS] = indexT + 1;
+            }
+
+            if(mapTS[indexT] != 0){
+                if(mapTS[indexT] != indexS + 1){
+                    return false;
+                }
+            } else {
+                // default 0, so the value should be index + 1
+                // a - a
+                // 0 - 1
+                mapTS[indexT] = indexS + 1;
             }
         }
 
