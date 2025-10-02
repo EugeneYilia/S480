94b7edf6891a2a5ea8d0c317211046a8a96a0908
EugeneLiu <583076221@qq.com>
2025-10-03 02:11:25 +0800

    surrounded Area

--- src/main/kotlin/yichen/ChessGo2.kt
+++ src/main/kotlin/yichen/ChessGo2.kt
@@ -1,54 +1,89 @@
 package yichen
 
 class ChessGo2 {
-    class positionNode {
-        var row = 0
-        var column = 0
-        var isVisited = false
-    }
+    fun solve(board: Array<CharArray>) {
+        val rows = board.size
+        val columns = board[0].size
 
-    fun solve(board: Array<CharArray>): Unit {
-        val row = board.size
-        val column = board[0].size
+        val visited = Array(rows) { BooleanArray(columns) }
 
-        if (row < 2 || column < 2) {
+        if (rows < 2 || columns < 2) {
             return
         }
 
-        val rootNodes = HashSet<Pair<Int, Int>>()
-        val connectedNode = HashSet<Pair<Int, Int>>()
-
-        for (col in 0..<column) {
+        val rowBottomNodes = HashSet<Pair<Int, Int>>()
+        val rowMaxNodes = HashSet<Pair<Int, Int>>()
+        val columnBottomNodes = HashSet<Pair<Int, Int>>()
+        val columnMaxNodes = HashSet<Pair<Int, Int>>()
+        for (col in 1..<columns - 1) {
             if (board[0][col] == 'O') {
-                rootNodes.add(Pair(0, col))
+                rowBottomNodes.add(Pair(1, col))
             }
 
-            if (board[row - 1][col] == 'O') {
-                rootNodes.add(Pair(row - 1, col))
+            if (board[rows - 1][col] == 'O') {
+                rowMaxNodes.add(Pair(rows - 2, col))
             }
         }
 
-        for (r in 0..<row) {
+        for (r in 1..<rows - 1) {
             if (board[r][0] == 'O') {
-                rootNodes.add(Pair(r, 0))
+                columnBottomNodes.add(Pair(r, 1))
             }
 
-            if (board[r][column - 1] == 'O') {
-                rootNodes.add(Pair(r, column - 1))
+            if (board[r][columns - 1] == 'O') {
+                columnMaxNodes.add(Pair(r, columns - 2))
             }
         }
+
+        val connectedNode = HashSet<Pair<Int, Int>>()
 
         fun searchFromRootNode(node: Pair<Int, Int>) {
             val row = node.first
             val column = node.second
+
+            fun canBeAccess(row: Int, column: Int): Boolean {
+                if (row !in 1..<rows - 1) {
+                    return false
+                }
+
+                if (column !in 1..<columns - 1) {
+                    return false
+                }
+
+                if (visited[row][column]) {
+                    return false
+                }
+
+                visited[row][column] = true
+                return true
+            }
+
+            val canBeAccess = canBeAccess(row, column)
+            if (canBeAccess && board[row][column] == 'O') {
+                connectedNode.add(Pair(row, column))
+
+                searchFromRootNode(Pair(row + 1, column))
+                searchFromRootNode(Pair(row - 1, column))
+                searchFromRootNode(Pair(row, column + 1))
+                searchFromRootNode(Pair(row, column - 1))
+            }
         }
 
-        for (node in rootNodes) {
+        for (node in rowBottomNodes) {
+            searchFromRootNode(node)
+        }
+        for (node in rowMaxNodes) {
+            searchFromRootNode(node)
+        }
+        for (node in columnBottomNodes) {
+            searchFromRootNode(node)
+        }
+        for (node in columnMaxNodes) {
             searchFromRootNode(node)
         }
 
-        for (row in 1..<row - 1) {
-            for (col in 1..<column - 1) {
+        for (row in 1..<rows - 1) {
+            for (col in 1..<columns - 1) {
                 if (board[row][col] == 'O' && !connectedNode.contains(Pair(row, col))) {
                     board[row][col] = 'X'
                 }
