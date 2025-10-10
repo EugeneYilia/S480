f4b3705863e7147c78b4812a46e00f2573587da7
EugeneLiu <583076221@qq.com>
2025-10-10 16:43:21 +0800

    sudoku

--- src/main/java/Sudoku.java
+++ src/main/java/Sudoku.java
@@ -1,62 +1,25 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-
 public class Sudoku {
+    private final short[] rows = new short[9];
+    private final short[] cols = new short[9];
+    private final short[] boxes = new short[9];
 
-    private static final class Point {
-        int row;
-        int col;
-        // 已有元素
-        int existValues;
+    private final byte[] emptyPoints = new byte[81];
+    private byte emptyPointsSize = 0;
+    private byte filledPointsSize = 0;
 
-        public Point(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
+    //  1             2              3            4          5              6              7                8             9
+    // 00000001      00000010      00000100     00001000   00010000      00100000       01000000        10000000
+    private static final short allValueNum = 511;
 
-        @Override
-        public int hashCode() {
-            return 10 * row + col;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            return obj instanceof Point && (row == ((Point) obj).row) && col == (((Point) obj).col);
-        }
-    }
-
-    private static int[] rows = new int[9];
-    private static int[] cols = new int[9];
-    private static int[] boxes = new int[9];
-
-    private static HashSet<Point> leftPoints = new HashSet<>();
-    private static final int allValueNum = 511;
-
-    private static ArrayList<Integer> getPossibleNumbers(Point point) {
-        ArrayList<Integer> result = new ArrayList<>();
-        // 1111 ^ 1101 = 0010
-        //               可选元素结果集
-        int possibleNumber = allValueNum ^ point.existValues;
-        // 1 2 4 8 16 32
-        for (int i = 0; i < 9; i++) {
-            int mask = 1 << i;
-            if ((mask & possibleNumber) != 0) {
-                result.add(i + 1);
-            }
-        }
-
-        return result;
-    }
-
-    public static void checkMatrix(char[][] src) {
-        for (int i = 0; i < src.length; i++) {
-            for (int j = 0; j < src[0].length; j++) {
+    public void checkMatrix(char[][] src) {
+        for (byte i = 0; i < src.length; i++) {
+            for (byte j = 0; j < src[0].length; j++) {
                 char c = src[i][j];
                 if (c == '.') {
-                    leftPoints.add(new Point(i, j));
+                    emptyPoints[emptyPointsSize++] = (byte) (i * 10 + j);
                 } else {
                     // [0,8]
-                    int mask = 1 << (c - '1');
+                    short mask = (short) (1 << (c - '1'));
                     // 1 1
                     // 2 2
                     // 3 4
@@ -70,74 +33,90 @@ public class Sudoku {
     }
 
     public void solveSudoku(char[][] board) {
-        rows = new int[9];
-        cols = new int[9];
-        boxes = new int[9];
-        leftPoints = new HashSet<>();
-
         checkMatrix(board);
 
         solveSudokuInternal(board);
     }
 
-    private static Point searchMinChoicePoint() {
-        Point minPoint = null;
-        int existNumCount = Integer.MIN_VALUE;
+    public void solveSudokuInternal(char[][] board) {
+        byte curPoint = -1;
+        byte minPointIndex = -1;
+        byte existNumCount = Byte.MIN_VALUE;
+        short pointValue = -1;
 
-        for (Point point : leftPoints) {
-            int row = point.row;
-            int col = point.col;
+        // all 5   filled 3
+        // 0-4     0-2    3-4
+        for (byte i = filledPointsSize; i < emptyPointsSize; i++) {
+            byte point = emptyPoints[i];
+            byte row = (byte) (point / 10);
+            byte col = (byte) (point % 10);
 
             int possibleNums = rows[row] | cols[col] | boxes[row / 3 * 3 + col / 3];
             int possibleNumsCount = Integer.bitCount(possibleNums);
             if (possibleNumsCount > existNumCount) {
-                existNumCount = possibleNumsCount;
-                minPoint = point;
-                minPoint.existValues = possibleNums;
+                minPointIndex = i;
+                existNumCount = (byte) possibleNumsCount;
+                curPoint = point;
+                pointValue = (short) possibleNums;
             }
         }
 
-        return minPoint;
-    }
+        emptyPoints[minPointIndex] = emptyPoints[filledPointsSize];
+        emptyPoints[filledPointsSize] = curPoint;
 
-    public void solveSudokuInternal(char[][] board) {
-        Point curPoint = searchMinChoicePoint();
-        leftPoints.remove(curPoint);
+        filledPointsSize++;
 
-        int rowStart = curPoint.row;
-        int columnStart = curPoint.col;
-        ArrayList<Integer> possibleNumbers = getPossibleNumbers(curPoint);
+        byte row = (byte) (curPoint / 10);
+        byte col = (byte) (curPoint % 10);
 
-        for (Integer possibleNumber : possibleNumbers) {
-            board[rowStart][columnStart] = (char)(possibleNumber + '0');
+        // 1111 ^ 1101 = 0010
+        //               可选元素结果集
+        short possibleNumber = (short) (allValueNum ^ pointValue);
+        // 1 2 4 8 16 32
 
-            var newBitValue = 1 << (possibleNumber - 1);
-            rows[rowStart] |= newBitValue;
-            cols[columnStart] |= newBitValue;
-            boxes[rowStart / 3 * 3 + columnStart / 3] |= newBitValue;
+        for (byte i = 0; i < 9; i++) {
+            short newBitValue = (short) (1 << i);
+            if ((newBitValue & possibleNumber) != 0) {
+                board[row][col] = (char) (i + 1 + '0');
 
-            // 填当前的看看是否可以填满
-            if (leftPoints.isEmpty()) {
-                return;
+                rows[row] |= newBitValue;
+                cols[col] |= newBitValue;
+                boxes[row / 3 * 3 + col / 3] |= newBitValue;
+
+                // 填当前的看看是否可以填满
+                if (filledPointsSize == emptyPointsSize) {
+                    return;
+                }
+
+                // 递归下去寻找解决方案
+                solveSudokuInternal(board);
+
+                // 递归下去如果填满了直接返回，不用进入到下面的回溯部分
+                if (filledPointsSize == emptyPointsSize) {
+                    return;
+                }
+
+                // 4 =   0000100
+                // ~4 =  1111011
+                short backTrackNum = (short) ~newBitValue;
+                rows[row] &= backTrackNum;
+                cols[col] &= backTrackNum;
+                boxes[row / 3 * 3 + col / 3] &= backTrackNum;
             }
-
-            // 递归下去寻找解决方案
-            solveSudokuInternal(board);
-
-            // 递归下去如果填满了直接返回，不用进入到下面的回溯部分
-            if (leftPoints.isEmpty()) {
-                return;
-            }
-
-            // 4 =   0000100
-            // ~4 =  1111011
-            int backTrackNum = ~newBitValue;
-            rows[rowStart] &= backTrackNum;
-            cols[columnStart] &= backTrackNum;
-            boxes[rowStart / 3 * 3 + columnStart / 3] &= backTrackNum;
         }
 
-        board[rowStart][columnStart] = '.';
-        leftPoints.add(curPoint);
+        board[row][col] = '.';
+        filledPointsSize--;
     }
 }
+
+
+// 2，2   0
+// 2，5   1
+// 2，8   2
+// 5，2   3
+// 5，5   4
+// 5，8   5
+// 8，2   6
+// 8，5   7
+// 8，8   8
